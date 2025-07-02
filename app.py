fdca131a5cd23c2f2b96de5d5833ac2c029f2990
Ea125 <altmaneviatar@gmail.com>
2025-07-02 22:43:18 +0300

    feat: created another decorators

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import request
-
+from flask import Flask, request, jsonify
+import decorators
+from decorators import log_call, timer, delay
 
 #mock db
 mock_users = {
@@ -14,11 +14,46 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@log_call
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/users', methods=['GET', 'POST'])
+@log_call
+@timer
+@delay(rate=1)
+def users():
+    if request.method == 'GET':
+        user_id = int(request.args.get('user_id'))
+        if not user_id:
+            return jsonify({'error': 'No user id provided'}), 400
+
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            return jsonify({'error': 'Invalid user id'}), 400
+
+        if user_id in mock_users:
+            return jsonify(mock_users[user_id]), 200
+        else:
+            return jsonify({"error":"user not found"}), 404
+    elif request.method == 'POST':
+        data = request.get_json()
+        if not data or "username" not in data or "email" not in data:
+            return jsonify({"error": "username and email are required"}), 400
+        new_user = {"username": data["username"], "email": data["email"]}
+        new_id = max(mock_users.keys()) + 1
+        users[new_id] = new_user
+        return jsonify({"id" : new_id, "username" : data["username"], "email": data["email"]}), 201
+
+
+
+
 
 @app.route('/echo', methods=['POST'])
+@log_call
+@timer
+@delay(rate=1)
 def echo():
     data = request.get_json()  # get JSON body
     return {"you_sent": data}, 200
--- decorators.py
+++ decorators.py
@@ -1,5 +1,29 @@
 import functools
+import time
+from time import sleep
 
+"""
+template
+"""
+def template(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return wrapper
+
+"""
+template with args
+"""
+
+def template_with_args(_func=None, *, key1=1, key2=2):
+    def decorator_name(func):
+        ...  # Create and return a wrapper function.
+
+    if _func is None:
+        return decorator_name
+    else:
+        return decorator_name(_func)
 
 """
 log function which implemented by as decorator
@@ -12,3 +36,33 @@ def log_call(func):
         print(f'Calling {func_name} with args=({args}) kwargs=({kwargs})')
         return func(*args, **kwargs)
     return wrapper
+
+"""
+timing the time it took to execute the function
+"""
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(f'{func.__name__} took {elapsed_time:0.4f} seconds')
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def delay(_func=None, *, rate=1):
+    def slow_down(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            time.sleep(rate)
+            func(*args, **kwargs)
+            return func(*args, **kwargs)
+        return wrapper
+
+    if _func is None:
+        return slow_down
+    else:
+        return slow_down(_func)
