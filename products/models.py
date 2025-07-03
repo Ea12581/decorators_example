c0410ebb991ec8e25b703c27246bf338a6b67e05
Ea125 <altmaneviatar@gmail.com>
2025-07-03 12:46:13 +0300

    ref: add decorators to products methods

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 #mock db
+from decorators.decorators import log_call, timer, delay
+
 mock_products = {
     "espresso": {"name": "Espresso", "price": 2.50},
     "cappuccino": {"name": "Cappuccino", "price": 3.50},
@@ -8,14 +10,22 @@ mock_products = {
     "flat_white": {"name": "Flat White", "price": 3.80},
 }
 
-
-def get_product(product_name):
+@log_call
+@timer
+@delay(rate=1)
+def get_product_by_name(product_name):
     return mock_products[product_name]
 
+@log_call
+@timer
+@delay(rate=1)
 def is_product_exists(product_name):
     return product_name in mock_products.keys()
 
-def add_product(name, cost):
+@log_call
+@timer
+@delay(rate=1)
+def add_new_product(name, cost):
     new_product = {"name": name, "cost": cost}
     mock_products[name] = new_product
     return new_product
